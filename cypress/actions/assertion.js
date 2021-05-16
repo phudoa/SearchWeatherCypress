5190610c1869cadf8bcc706dfb78dd8c877c19fd
phuong <phuongdoan@nhatvietgroup.com.vn>
2021-05-16 11:44:01 +0700

    update the assertion checkpoints

--- cypress/actions/assertion.js
+++ cypress/actions/assertion.js
@@ -1,60 +1,56 @@
-Cypress.Commands.add('verifyWeatherOfYourCity',(city)=>{
-    let parameters= [`q=${city}`]
+function getNumberInString(string, startStr, endStr){
+    let idxStart = string.indexOf(startStr)
+    let idxEnd = string.indexOf(endStr)
+    let tempString = string.slice(idxStart, idxEnd)
+    return parseFloat(tempString.replace(/[^0-9.]/g, " "))
+}
+
+Cypress.Commands.add('verifyWeatherOfYourCity', (yourCity) => {
+    let parameters = [`q=${yourCity}`]
     //get the current weather info of the city by API
-    cy.apiWeatherByCity(parameters).then(res=>{
+    cy.apiWeatherByCity(parameters).then(res => {
         let cityName = res.body["name"]
         let countryCode = res.body.sys["country"]
         let desc = res.body.weather[0].description
         let temp = res.body.main["temp"]
-        let temp_min = res.body.main["temp_min"]
-        let temp_max = res.body.main["temp_max"]
+        let tempMin = res.body.main["temp_min"]
+        let tempMax = res.body.main["temp_max"]
         let wind = res.body.wind["speed"]
         let clouds = res.body.clouds["all"]
-        let pressue= res.body.main["pressure"]
+        let pressue = res.body.main["pressure"]
         let lat = res.body.coord["lat"]
         let lon = res.body.coord["lon"]
 
-        //verify the results
-        cy.get('tr').within(()=>{
+        //verify the results of a city
+        cy.get('tr').within(() => {
+            //verify the weather icon
+            cy.get('td > img').should("be.visible")
             cy.get('td').eq(1)
-                        .should("be.visible")
-                        .within(()=>{
-                            cy.get('b').eq(0).should('contain', `${cityName}, ${countryCode}`)
-                            cy.get('b').eq(1).should('contain', desc)
-                            cy.get('p').eq(0).should('contain',`wind ${wind} m/s. clouds ${clouds} %, ${pressue} hpa`)
-                            cy.get('p').eq(1).should('have.text',`Geo coords [${lat}, ${lon}]`)
-                            
-                            // cy.get('p > span').then($element=>{
-                            //     let myTemp = parseFloat($element.text().split("°С "))
-                            //     expect(myTemp).to.be.within(temp -1, temp +1)
-                            // })
-                            cy.get('p').eq(0).then($element=>{
-                                let string = $element.text().replace(/[^0-9.]/g," ",10)
-                                cy.log("String --"+ string)
-                                let newString = string.split(' ')
-                                cy.log("New String --"+ newString)
-                                let finalarr =[]
-                                cy.wrap(newString).each(item=>{
-                                    if(item!="" && item!="." )
-                                    {
-                                        finalarr.push(item)
-                                        cy.log(item)
-                                    }
-                                        
-                                })
+                .should("be.visible")
+                .within(() => {
+                    //verify the cityname, country code
+                    cy.get('b').eq(0).should('contain', `${cityName}, ${countryCode}`)
+                    //verify the weather description
+                    cy.get('b').eq(1).should('contain', desc)
+                    //verify the geo coords (lat, lon)
+                    cy.get('p').eq(1).should('have.text', `Geo coords [${lat}, ${lon}]`)
+                    //verify the weather info : wind, clouds, pressure
+                    cy.get('p').eq(0).should('contain', `wind ${wind} m/s. clouds ${clouds} %, ${pressue} hpa`)
 
-                                // expect(finalarr[0]).to.be.within(temp -1, temp +1)
-                                // expect(finalarr[1]).to.be.within(temp_min -1, temp_min +1)
-                                // expect(finalarr[2]).to.be.within(temp_max -1, temp_max +1)
+                    //verify current temp (approximation +/- 1)
+                    cy.get('p > span').then($element=>{
+                        let myTemp = parseFloat($element.text().split("°С "))
+                        expect(myTemp).to.be.within(temp -1, temp +1)
+                    })
 
-                                // let myText = $element.text()
-                                // let start = myText.indexOf("°С")
-                                // let end = myText.indexOf(",")
-                                // let tempMinMax = myText.slice(start + 2, end)
-                                // expect(tempMinMax).to.have.string(` temperature from 10.6 to 11.1 °С`)
-                            })
-                            
-                        })
+                    //verify current temp_min, temp_max (approximation +/- 1)
+                    cy.get('p').eq(0).then($element => {
+                        let tempMinActual = getNumberInString ($element.text(), "from", "to")
+                        expect(tempMinActual).to.be.within(tempMin -1, tempMin +1)
+                        let tempMaxActual = getNumberInString ($element.text(), "to", "wind")
+                        expect(tempMaxActual).to.be.within(tempMax -1, tempMax +1)
+                    })
+                })
         })
     })
 })
--- cypress/tests/layout.spec.js
+++ cypress/tests/layout.spec.js
@@ -1,34 +1,22 @@
 import * as iSearch from '../interface/isearch'
 
 context('Search Weather Feature',()=>{
-    //Verify the search Feature layout on mobile screens
-    const screenMobileSizes = ['iphone-5', 'samsung-s10', 'iphone-x', 'ipad-mini']//, 'macbook-13'];  
+    //Verify the search Feature layout on responsive design
+    const screenMobileSizes = ['iphone-5', 'samsung-s10', 'iphone-x', 'ipad-mini', [1366, 768], [1440, 900]];  
     screenMobileSizes.forEach(size => {
         it(`SER_007 - Verify the search feature on the menu on ${size} design`,()=>{
             if (Cypress._.isArray(size)) {
                 cy.viewport(size[0], size[1])
+                cy.goToWebSite()
+                cy.get(iSearch.iconSearch).should("be.visible")
+                cy.get(iSearch.desktopMenu).matchImageSnapshot()
             } else {
                 cy.viewport(size)
+                cy.goToWebSite()
+                cy.get(iSearch.menu).should("be.visible").click().wait(500)
+                cy.get(iSearch.iconSearch).should("be.visible")
+                cy.get(iSearch.mobileMenu).matchImageSnapshot()
             }
-            cy.goToWebSite()
-            cy.get(iSearch.menu).should("be.visible").click()
-            cy.get(iSearch.iconSearch).should("be.visible")
-            cy.get(iSearch.mobileMenu).matchImageSnapshot()
-        })
-    })
-
-    //Verify the search Feature layout on desktop screens
-    const screenDesktopSizes = ['macbook-13', 'macbook-15']
-    screenDesktopSizes.forEach(size => {
-        it(`SER_007 - Verify the search feature on the menu on ${size} design`,()=>{
-            if (Cypress._.isArray(size)) {
-                cy.viewport(size[0], size[1])
-            } else {
-                cy.viewport(size)
-            }
-            cy.goToWebSite()
-            cy.get(iSearch.iconSearch).should("be.visible")
-            cy.get(iSearch.desktopMenu).matchImageSnapshot()
         })
     })
 })
